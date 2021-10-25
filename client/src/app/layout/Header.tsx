29e77a99ff745930596bb33a945c952c2b82d6cf
Irvin M. Waldman <irvin.waldman@hotmail.com>
2021-10-24 19:50:57 -0500

    Add AppBar

---
+++ client/src/app/layout/Header.tsx
@@ -0,0 +1,12 @@
+import { AppBar, Typography } from '@mui/material'
+import React from 'react'
+
+function Header() {
+  return (
+    <AppBar position="static">
+      <Typography variant="h6">RE_STORE</Typography>
+    </AppBar>
+  )
+}
+
+export default Header
