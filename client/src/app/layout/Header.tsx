23b1a234b7540ba6e5326588fdfdf421c88c0ea3
Irvin M. Waldman <irvin.waldman@hotmail.com>
2021-10-24 20:03:35 -0500

    Add cards for our individual products

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ mb: 4 }}>
       <Typography variant="h6">RE_STORE</Typography>
     </AppBar>
   )
