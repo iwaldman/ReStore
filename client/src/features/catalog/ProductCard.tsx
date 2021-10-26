44f86fd4336320c80de307ba99a6fb6aa25dc56c
Irvin M. Waldman <irvin.waldman@hotmail.com>
2021-10-25 22:28:46 -0500

    Fetch a product on component load

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -9,6 +9,7 @@ import {
   Typography,
 } from '@mui/material'
 import { fontWeight } from '@mui/system'
+import { Link } from 'react-router-dom'
 
 import { Product } from '../../app/models/product'
 
@@ -53,7 +54,9 @@ function ProductCard({ product }: Props) {
       </CardContent>
       <CardActions>
         <Button size="small">Add to cart</Button>
-        <Button size="small">View</Button>
+        <Button size="small" component={Link} to={`/catalog/${product.id}`}>
+          View
+        </Button>
       </CardActions>
     </Card>
   )
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,7 +1,31 @@
 import { Typography } from '@mui/material'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { Product } from '../../app/models/product'
 
 function ProductDetails() {
-  return <Typography variant="h2">Product Details</Typography>
+  const { id } = useParams<{ id: string }>()
+  const [product, setProduct] = useState<Product | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/products/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false))
+  }, [id])
+
+  if (loading) {
+    return <h3>Loading...</h3>
+  }
+
+  if (!product) {
+    return <h3>Product not found.</h3>
+  }
+
+  return <Typography variant="h2">{product.name}</Typography>
 }
 
 export default ProductDetails
